f99f08ef583ca78b99f39373b57926bd9c68eacd
Max <maksimli722@gmail.com>
2024-12-05 20:06:29 +0500

    first commit

--- src/main/java/com/example/store/exceptions/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.store.exceptions;
-
-public class ResourceNotFoundException extends Exception {
-    private static final long serialVersionUID = 1L;
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
